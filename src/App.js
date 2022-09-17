e08d88648cd35deeee44834ab5b04c204d6eef52
Anigbo Christopher <octan311@gmail.com>
2022-09-17 08:44:03 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import firebase from "./firebase";
 import './App.css';
+import Home from './components/Home';
+import Register from './components/Register';
+import LogIn from './components/LogIn';
 
 function App() {
+
+  const [user,  setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
+
+// this is the log in fuction
+  const handleLogin = () => {
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(err => {
+          switch(err.code) {
+            case "auth/invalid-email":
+            case "auth/user-disabled":
+            case "auth/user-not-found":
+              setEmailError(err.message);
+              break;
+            case "auth/wrong-password":
+              setPasswordError(err.message);
+              break;
+          }
+        });
+  };
+
+  //this is the sign up fuction
+  const handleSignup = () => {
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch(err => {
+          switch(err.code) {
+            case "auth/invalid-email":
+            case "auth/user-disabled":
+            case "auth/user-not-found":
+              setEmailError(err.message);
+              break;
+            case "auth/wrong-password":
+              setPasswordError(err.message);
+              break;
+          }
+        });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7oEQZ-Z69B1h00wwbQmiRUQwgCUhno9o",
+    authDomain: "vr-a-794e2.firebaseapp.com",
+    projectId: "vr-a-794e2",
+    storageBucket: "vr-a-794e2.appspot.com",
+    messagingSenderId: "992955504232",
+    appId: "1:992955504232:web:b82ba5b256ea0be8270ddd"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
+
